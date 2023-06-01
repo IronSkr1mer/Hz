c5d02746b857243a2ea1d8cf012199372811ed4c
Hp <ironskrimer228@gmail.com>
2023-06-01 10:34:36 +0300

    da

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,21 @@
-public class Test {
+public class Test
+{
+
+    public static void main(String[] args)
+    {
+
+        int a = 5;
+        int b = 4;
+        int c = a + b;
+        int f = a - b;
+        int g = a * b;
+        float h = a / b;
+        System.out.println(a + "+" + b + "=" + c );
+        System.out.println(a + "-" + b + "=" + f );
+        System.out.println(a + "*" + b + "=" + g );
+        System.out.println(a + "/" + b + "=" + h );
+
+
+    }
+
 }
