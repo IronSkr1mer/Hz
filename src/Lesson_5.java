046d7a2c348bb6e8f599359cfe4fdbff5fb4ea45
Hp <ironskrimer228@gmail.com>
2023-06-06 20:58:52 +0300

    da

--- src/Lesson_5.java
+++ src/Lesson_5.java
@@ -1,2 +1,16 @@
-public class Lesson_5 {
+import javax.swing.*;
+
+public class Lesson_5
+{
+    int a = 4;
+    int b = 6;
+
+    if (a > b)
+    {
+        System.out.println(a + ">" + b);
+    }
+    else
+    {
+        System.out.println(a + "<" + b);
+    }
 }
