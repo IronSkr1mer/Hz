04e6c97cc03c01ca929445a0d1fe9e8d7d504cb7
IronSkr1mer <134776182+IronSkr1mer@users.noreply.github.com>
2023-06-06 21:44:41 +0300

    Update Lesson_4.java

--- src/Lesson_4.java
+++ src/Lesson_4.java
@@ -11,7 +11,6 @@ public class Lesson_4 {
         char firstChar = text.charAt(0);
         System.out.println("Перша літера: " + firstChar);
 
-        // Остання літера
         char lastChar = text.charAt(text.length() - 1);
         System.out.println("Остання літера: " + lastChar);
     }
